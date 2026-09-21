c14ebf835a6d0d291a3ae218b307ca457b114ebd
kpeerasak-arch <k.peerasak@gmail.com>
2026-09-22 06:11:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbz.../exec', // แทนที่ด้วย Web App URL จริง
+  API_URL: 'https://script.google.com/macros/s/AKfycbz831Jxm1C6u64yq0FaWcGVneLXwxp9LGt88643WS1NhVixJGT474DL_CJ23_VmQW8o/exec', // แทนที่ด้วย Web App URL จริง
   API_TOKEN: 'SPORTS_SCI_SECURE_TOKEN_2026_XyZ99',
   APP_NAME: 'ระบบบริหารจัดการศูนย์วิทยาศาสตร์การกีฬาและฟิตเนส',
   DEVELOPER: 'Dr.peerasak kingpoum',
